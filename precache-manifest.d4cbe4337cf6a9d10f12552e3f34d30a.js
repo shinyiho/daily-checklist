8d1eded040e548c0b43ee0b4799103a98605758d
shinyiho <gbb2237@gmail.com>
2021-03-04 11:38:29 -0500

    Updates

---
+++ precache-manifest.d4cbe4337cf6a9d10f12552e3f34d30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94c121ff8a2bd800e6c6cf6152354ff",
+    "url": "/daily-checklist/index.html"
+  },
+  {
+    "revision": "5cbd79b52d25638c547f",
+    "url": "/daily-checklist/static/css/main.0a394106.chunk.css"
+  },
+  {
+    "revision": "170db398f9ccd34d584b",
+    "url": "/daily-checklist/static/js/2.d4efae4a.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/daily-checklist/static/js/2.d4efae4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbd79b52d25638c547f",
+    "url": "/daily-checklist/static/js/main.313f4a0d.chunk.js"
+  },
+  {
+    "revision": "81926f668d332d65ee12",
+    "url": "/daily-checklist/static/js/runtime-main.fc93b305.js"
+  }
+]);
